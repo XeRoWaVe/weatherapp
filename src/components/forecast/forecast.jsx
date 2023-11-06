924b5167285500df374bc57b16ded6469988d900
Aaron Craig <xerowave22@hotmail.com>
2023-11-06 08:35:32 -0600

    forecast functionality

--- src/components/forecast/forecast.jsx
+++ src/components/forecast/forecast.jsx
@@ -4,22 +4,47 @@ import {
   AccordionItemButton,
   AccordionItemHeading,
   AccordionItemPanel,
- } from "react-accessible-accordion";
+} from "react-accessible-accordion";
+
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 
 const Forecast = ({ data }) => {
+  const dayInAWeek = new Date().getDay();
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
   return (
     <>
       <label className="title">Daily</label>
       <Accordion allowZeroExpanded>
         {data.list.slice(0, 7).map((item, idx) => {
-          <AccordionItem key={idx}>
-            <AccordionItemHeading>
-              <AccordionItemButton>
-                Hello
-              </AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
-          </AccordionItem>
+          return (
+            <AccordionItem key={idx}>
+              <AccordionItemHeading>
+                <AccordionItemButton>
+                  <div className="daily-item">
+                    <img
+                      alt="weather"
+                      className="icon-small"
+                      src={`src/assets/${item.weather[0].icon}.png`}
+                    />
+                    <label className="day">{forecastDays[idx]}</label>
+                    <label className="description">{item.weather[0].description.charAt(0).toUpperCase() + item.weather[0].description.slice(1)}</label>
+                    <label className="min-max">{Math.round(item.main.temp_min)}ºF / {Math.round(item.main.temp_max)}ºF</label>
+                  </div>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel></AccordionItemPanel>
+            </AccordionItem>
+          );
         })}
       </Accordion>
     </>
